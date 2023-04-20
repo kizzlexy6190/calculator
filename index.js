651188003809f179245d2a9c39115f6007e0c692
olamilekan <alqasim6190@gmail.com>
2023-04-20 11:04:59 +0100

    finished responsiveness

---
+++ index.js
@@ -0,0 +1,46 @@
+function deleteCal(){
+    document.getElementById('output').value = ""
+}
+function myFunction(){
+    console.clear();
+}
+
+function one(){
+    document.getElementById('output').value ='1'
+}
+function two(){
+    document.getElementById('output').value ='2'
+}
+function three(){
+    document.getElementById('output').value ='3'
+}
+function four(){
+    document.getElementById('output').value ='4'
+}
+function five(){
+    document.getElementById('output').value ='5'
+}
+function six(){
+    document.getElementById('output').value ='6'
+}
+function seven(){
+    document.getElementById('output').value ='7'
+}
+function eight(){
+    document.getElementById('output').value ='8'
+}
+function nine(){
+    document.getElementById('output').value ='9'
+}
+function zero(){
+    document.getElementById('output').value ='0'
+}
+
+
+
+
+
+
+
+
+
